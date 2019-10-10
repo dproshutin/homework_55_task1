6cd98245672b60702b7addc3c82697cf42c28d89
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-10-10 15:06:50 +0600

    Time spent for frontend tasks is calculated.

---
+++ index.js
@@ -0,0 +1,20 @@
+const tasks = [
+    {id: 234, title: 'Create user registration API', timeSpent: 4, category: 'Backend', type: 'task'},
+    {id: 235, title: 'Create user registration UI', timeSpent: 8, category: 'Frontend', type: 'task'},
+    {id: 237, title: 'User sign-in via Google UI', timeSpent: 3.5, category: 'Frontend', type: 'task'},
+    {id: 238, title: 'User sign-in via Google API', timeSpent: 5, category: 'Backend', type: 'task'},
+    {id: 241, title: 'Fix account linking', timeSpent: 5, category: 'Backend', type: 'bug'},
+    {id: 250, title: 'Fix wrong time created on new record', timeSpent: 1, category: 'Backend', type: 'bug'},
+    {id: 262, title: 'Fix sign-in failed messages', timeSpent: 2, category: 'Frontend', type: 'bug'},
+];
+
+const tasksFrontend = tasks.filter(item => {
+    return item.category === 'Frontend';
+});
+let timeSpentForFrontend = tasksFrontend.map(item => {
+    return item.timeSpent;
+});
+timeSpentForFrontend = timeSpentForFrontend.reduce((acc, item) => {
+        return acc + item;
+    });
+console.log("Total time spent for frontend tasks:", timeSpentForFrontend);
